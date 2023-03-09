63c6430f2ae1f1306b652e87dc486e4ed86115d0
HARSAVARDINI-KS <127215584+HARSAVARDINI-KS@users.noreply.github.com>
2023-03-09 14:56:45 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,7 @@
+x=int(input("Enter a value for x:"))
+y=int(input("Enter a value for y:"))
+temp=x
+x=y
+y=temp
+print("Value of x After Swapping:{}".format(x))
+print("Value of y After Swapping:{}".format(y))
